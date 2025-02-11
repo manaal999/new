4f412831a0ceac10267f10ad43cdf96dfdf642f2
manaal999 <manaalkounain0786@gmail.com>
2025-02-11 13:02:10 +0530

    hi

---
+++ day2react/examples/src/App.jsx
@@ -0,0 +1,9 @@
+import Greetings from "./Greetings"
+ export default function APP() {
+return (
+  <>
+<h1><marquee>Welcome to REACT</marquee></h1>
+<Greetings/>
+</>
+);
+ }
---
+++ day2react/examples/src/Greetings.jsx
@@ -0,0 +1,8 @@
+function Greetings(){
+    return(
+        <>
+        <h3><marquee>Hi i am greeting child component</marquee></h3>
+        </>
+    )
+}
+export default Greetings;
